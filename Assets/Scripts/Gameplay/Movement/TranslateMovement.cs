282f2e5ff756ffd5ce4566d94fa29f4dc9d34dea
Zakhar Pustovgarov <38427894+ZakharPustovgarov@users.noreply.github.com>
2025-09-15 10:23:05 +0300

    Changed movement space from self to world

--- Assets/Scripts/Gameplay/Movement/TranslateMovement.cs
+++ Assets/Scripts/Gameplay/Movement/TranslateMovement.cs
@@ -4,6 +4,6 @@ public class TranslateMovement : Movement
 {
     public override void Move(Vector3 direction)
     {
-        this.transform.Translate(direction * speedMultiplier * Time.deltaTime);
+        this.transform.Translate(direction * speedMultiplier * Time.deltaTime, Space.World);
     }
 }
